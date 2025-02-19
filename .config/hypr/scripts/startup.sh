86ce703542e3fedbcfe1cb039a3e2f5186a82fad
Helpfullinx <zachvdub2002@gmail.com>
2025-02-19 16:35:06 +0000

    added nixos config and oh my posh config

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 waybar &
 swaync &
-/usr/libexec/polkit-gnome-authentication-agent-1 &
+soteria &
 
 swww-daemon &
 swww img "$HOME/.config/hypr/wallpapers/8.png" --transition-type simple --transition-duration 3 &
